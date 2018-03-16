d89b7a18031a1d3571729480ba3477e7bbe3edcc
chtld <zhangshangyuan1357@gmail.com>
2018-03-16 10:03:01 +0800

    add netwon method

--- Secant_method_test.m
+++ Secant_method_test.m
@@ -2,15 +2,15 @@ format long;
 %f=@(x) cos(x)-x;
 f=@(x) cos(x);
 left = 0;
-right = 1.6;
+right = 1;
 epsilon = 0.0001;
-[root, iterations, tol] = Inverse_Linear_Interpolation(f, left, right, epsilon);
+[root, iterations, tol] = Secant(f, left, right, epsilon);
 fprintf('The solution of this equation is : %f\n',root);
 fprintf('The number of this iterations is : %d\n',iterations);
 fprintf('The length of the interval is : %f\n',tol);
 
 
-function [result, iterations, tol] = Inverse_Linear_Interpolation(function_f, left, right, epsilon)
+function [result, iterations, tol] = Secant(function_f, left, right, epsilon)
 % This function has four input parameter, and return the root of f(x)=0
 % input:
 %     function_f is the function f(x) = 0 you want to solve
@@ -37,7 +37,12 @@ function [result, iterations, tol] = Inverse_Linear_Interpolation(function_f, le
     end
     iterations = 1;
     while(abs(f(x2))>epsilon)
-        x0 = x2;
+        if(x2>x1)
+            x0 = x1;
+            x1 = x2;
+        else
+            x0 = x2;
+        end
         x2 = x1-f(x1)*(x1-x0)/(f(x1)-f(x0));
         iterations = iterations + 1;
     end
--- fixed_point.m
+++ fixed_point.m
@@ -8,29 +8,27 @@ format long;
 %f = @(x) x^2;
 %f = @(x) atan(x);
 f = @(x) x^3+x^2-3*x-3;
-f1 = @(x) x-(x^3+x^2-3)/3;
-f2 = @(x) x-(-1+(3*x+3)/x^2);
-f3 = @(x) x-(3+3*x-x^2)^(1/3);
-f4 = @(x) x-((3+3*x-x^3)/x)^(1/2);
-f5 = @(x) x-(x-(x^3+x^2-3*x-3)/(3*x^2+2*x-3));
+f1 = @(x) (x^3+x^2-3)/3;
+f2 = @(x) (-1+(3*x+3)/x^2);
+f3 = @(x) (3+3*x-x^2)^(1/3);
+f4 = @(x) ((3+3*x-x^2)/x)^(1/2);
+f5 = @(x) (x-(x^3+x^2-3*x-3)/(3*x^2+2*x-3));
 x0 = 1.;
 epsilon = 1e-6;
-max_iterations = 1000;
-root = Newton_Method(f, x0, epsilon, max_iterations);
+max_iterations = 100;
+root = fixedPoint(f1, x0, epsilon, max_iterations);
 fprintf('The solution of this equation is : %f\n',root);
-root = Newton_Method(f1, x0, epsilon, max_iterations);
+root = fixedPoint(f2, x0, epsilon, max_iterations);
 fprintf('The solution of this equation is : %f\n',root);
-root = Newton_Method(f2, x0, epsilon, max_iterations);
+root = fixedPoint(f3, x0, epsilon, max_iterations);
 fprintf('The solution of this equation is : %f\n',root);
-root = Newton_Method(f3, x0, epsilon, max_iterations);
+root = fixedPoint(f4, x0, epsilon, max_iterations);
 fprintf('The solution of this equation is : %f\n',root);
-root = Newton_Method(f4, x0, epsilon, max_iterations);
-fprintf('The solution of this equation is : %f\n',root);
-root = Newton_Method(f5, x0, epsilon, max_iterations);
+root = fixedPoint(f5, x0, epsilon, max_iterations);
 fprintf('The solution of this equation is : %f\n',root);
 
 
-function [result, iterations] = Newton_Method(function_f, x0, epsilon, max_iterations)
+function [result, iterations] = fixedPoint(function_f, x0, epsilon, max_iterations)
 % This function has four input parameter, and return the root of f(x)=0
 % input:
 %     function_f is the function f(x) = 0 you want to solve
@@ -40,18 +38,18 @@ function [result, iterations] = Newton_Method(function_f, x0, epsilon, max_itera
 % output:
 %     result is the root of the equation f(x)=0
 %     iterations is the number of iterations
-    syms x;
-    f = function_f(x);%匿名函数转符号函数
-    g = diff(f, 1);
-    f = matlabFunction(f);%符号函数转匿名函数
-    g = matlabFunction(g);
+%     syms x;
+    f = function_f;
+%     g = diff(f, 1);
+%     f = matlabFunction(f);%符号函数转匿名函数
+%     g = matlabFunction(g);
     iterations = 1;
     fprintf('iterations      result\n');
-    x1 = x0 - feval(f,x0)/feval(g,x0);
+    x1 = feval(f,x0);
     fprintf('%d     %f\n',0,x0);
-    while(abs(x1 - x0) > epsilon && iterations < max_iterations && f(x0)~=0)
+    while(abs(x1 - x0) > epsilon && iterations < max_iterations)
         x0 = x1;
-        x1 = x0 - feval(f,x0)/feval(g,x0);
+        x1 = feval(f,x0);
         fprintf('%d     %f\n',iterations,x0);
         iterations = iterations+1;
     end
